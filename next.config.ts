a293c464401d068cffbea4dc1a0c0ad3382516e7
Htc-wh <161570036+Htc-wh@users.noreply.github.com>
2026-01-13 11:17:08 +0800

    优化配置以支持 Zeabur 部署

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
   typescript: {
     // ignoreBuildErrors: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
